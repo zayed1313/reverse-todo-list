4e5c4cda7b0d79aa9051735be08778c12421cf8c
zayed1313 <zayed.salim@efrei.net>
2024-11-27 15:20:32 +0100

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require("./routes/tasks");
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const mongoURI = process.env.MONGO_URL || "mongodb+srv://zayed:iglpAv7ZSyiwy2lI@cluster0.djw2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use('/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
